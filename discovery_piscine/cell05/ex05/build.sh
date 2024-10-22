f3f2a9672229efed5405d1b168488e6bfeb47c55
Sananchx <Sathaphat.jet@gmail.com>
2024-10-22 11:11:09 +0700

    Update build.sh

--- discovery_piscine/cell05/ex05/build.sh
+++ discovery_piscine/cell05/ex05/build.sh
@@ -1,1 +1,17 @@
+$>ls -1
+-rwxr-xr-x 1 will user 42 42 juin 14 23.42 build.sh
+$>./build.sh
+No arguments supplied
+$>./build.sh 00
+$>ls -1
+-rwxr-xr-x 1 wil user42 42 juin 14 23:42 build.sh
+drwxr-xr-x 2 will user42 4,OK juin 14 23:42 ex00
+$>./build.sh 01 02 03 04
+$>ls -1
+-rwxr-xr-x 1 wil user42 42 juin 14 23:42 build.sh
+drwxr-xr-x 2 wil user42 4,OK juin 14 23:42 ex01
+drwxr-xr-x 2 wil user42 4,OK juin 14 23:42 ex02
+drwxr-xr-x 2 wil user42 4,OK juin 14 23:42 ex03
+drwxr-xr-x 2 wil user42 4,OK juin 14 23:42 ex04
+$>
 
